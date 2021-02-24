fe30edc41a28e65fcf3e29cd0d0fbfcadfbd5f61
Cong Ma <cong.ma@uct.ac.za>
2021-02-24 11:25:16 +0200

    MAINT: Reorder the members of LRUDict struct and give the struct a name.
    The members are reordered for (perceived) readability. Notice that
    currently the flags have some slop padding.

--- src/lrudict.c
+++ src/lrudict.c
@@ -99,21 +99,21 @@ do {                            \
 
 /* Implementation of LRUDict object */
 /* Object structure */
-typedef struct {
+typedef struct _LRUDict {
     PyObject_HEAD
-    PyObject *dict;
-    Node *first;
-    Node *last;
-    PyObject *callback;
-    PyObject *staging_list;
-    Py_ssize_t size;
-    unsigned long hits;
-    unsigned long misses;
     _Bool internal_busy:1;
     _Bool detect_conflict:1;
     _Bool should_purge;
     _Bool purge_busy:1;
     _Bool purge_suspended:1;
+    PyObject *staging_list;
+    PyObject *callback;
+    Node *first;
+    Node *last;
+    PyObject *dict;
+    Py_ssize_t size;
+    unsigned long hits;
+    unsigned long misses;
 } LRUDict;
 
 
